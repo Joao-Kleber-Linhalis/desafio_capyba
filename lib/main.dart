0b3bcd8a9da028a9e8945f7a5ab380af5727e5a8
João Kleber Linhalis <jklinhalis@gmail.com>
2025-04-14 13:49:18 -0300

    add provider pub

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:desafio_capyba/core/routes/app_routes.dart';
+import 'package:desafio_capyba/features/auth/provider/auth_provider.dart';
 import 'package:desafio_capyba/features/presentation/splash_screen.dart';
 import 'package:desafio_capyba/firebase_options.dart';
 import 'package:desafio_capyba/shared/constants/app_colors.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -18,13 +20,20 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme:
-          ThemeData(scaffoldBackgroundColor: AppColors.scaffoldBackgroundColor),
-      routes: {
-        AppRoutes.SPLASH: (context) => const SplashScreen(),
-      },
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (_) => AuthProvider(),
+        ),
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+            scaffoldBackgroundColor: AppColors.scaffoldBackgroundColor),
+        routes: {
+          AppRoutes.SPLASH: (context) => const SplashScreen(),
+        },
+      ),
     );
   }
 }
