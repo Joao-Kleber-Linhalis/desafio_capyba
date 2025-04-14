a3b01c041986c301f73fe367ad697a52061ec3ed
João Kleber Linhalis <jklinhalis@gmail.com>
2025-04-14 14:04:47 -0300

    Auth or home logic

---
+++ lib/features/presentation/auth_or_home_screen.dart
@@ -0,0 +1,35 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:desafio_capyba/features/auth/provider/auth_provider.dart'
+    as my_auth; // Já existe um AuthProvider no proprio provider, é necessário importar com outro nome para evitar uso do errado.
+
+class AuthOrHomeScreen extends StatefulWidget {
+  const AuthOrHomeScreen({super.key});
+
+  @override
+  State<AuthOrHomeScreen> createState() => _AuthOrHomeScreenState();
+}
+
+class _AuthOrHomeScreenState extends State<AuthOrHomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<User?>(
+      stream: my_auth.AuthProvider.authStateChanges(),
+      builder: (context, snapshot) {
+        // Verifica o estado da autenticação
+        if (snapshot.connectionState == ConnectionState.active) {
+          // Se o usuário estiver autenticado
+          if (snapshot.hasData) {
+            //return const HomeScreen(); // Tela inicial ou navegação para o app
+          } else {
+            //return AuthScreen();
+          }
+        }
+
+        return const Center(
+          child: CircularProgressIndicator(),
+        );
+      },
+    );
+  }
+}
