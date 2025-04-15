745fbca68a6c75a8e4d06a839f55e1885f5f45c9
João Kleber Linhalis <jklinhalis@gmail.com>
2025-04-15 13:49:18 -0300

    create new routes

--- lib/core/routes/app_routes.dart
+++ lib/core/routes/app_routes.dart
@@ -1,4 +1,7 @@
 class AppRoutes {
   static const SPLASH = '/';
   static const AUTH_OR_HOME = '/auth-or-home';
+  static const AUTH_SCREEN = '/auth-screen';
+  static const HOME_SCREEN = '/home-screen';
+  static const PROFILE_SCREEN = '/profile-screen';
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:desafio_capyba/core/routes/app_routes.dart';
+import 'package:desafio_capyba/features/auth/presentation/screens/auth_screen.dart';
 import 'package:desafio_capyba/features/auth/provider/auth_provider.dart';
 import 'package:desafio_capyba/features/presentation/auth_or_home_screen.dart';
 import 'package:desafio_capyba/features/presentation/splash_screen.dart';
+import 'package:desafio_capyba/features/profile/presentation/profile_screen.dart';
 import 'package:desafio_capyba/firebase_options.dart';
 import 'package:desafio_capyba/shared/constants/app_colors.dart';
 import 'package:firebase_core/firebase_core.dart';
@@ -34,6 +36,8 @@ class MyApp extends StatelessWidget {
         routes: {
           AppRoutes.SPLASH: (context) => const SplashScreen(),
           AppRoutes.AUTH_OR_HOME: (context) => const AuthOrHomeScreen(),
+          AppRoutes.AUTH_SCREEN: (context) => const AuthScreen(),
+          AppRoutes.PROFILE_SCREEN: (context) => const ProfileScreen(),
         },
       ),
     );
