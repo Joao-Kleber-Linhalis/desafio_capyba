6b6e8712dda51fed28881c5ca719769b4bef1a9c
João Kleber Linhalis <jklinhalis@gmail.com>
2025-04-18 15:36:26 -0300

    home e restricted models

---
+++ lib/features/home/models/home_model.dart
@@ -0,0 +1,71 @@
+import 'package:desafio_capyba/shared/constants/collections.dart';
+import 'package:desafio_capyba/shared/models/base_model.dart';
+
+class HomeModel extends BaseModel<HomeModel> {
+  final String id;
+  final String title;
+  final String description;
+  final String imageUrl;
+
+  HomeModel({
+    required this.id,
+    required this.title,
+    required this.description,
+    required this.imageUrl,
+  });
+
+  factory HomeModel.empty() {
+    return HomeModel(
+      id: "",
+      title: "",
+      description: "",
+      imageUrl: "",
+    );
+  }
+
+  @override
+  fromMap(Map<String, dynamic>? map) {
+    if (map == null) return HomeModel.empty();
+    return HomeModel(
+      id: map["id"] ?? "",
+      title: map['title'] ?? "",
+      description: map['description'] ?? "",
+      imageUrl: map['imageUrl'] ?? "",
+    );
+  }
+
+  HomeModel copyWith({
+    String? id,
+    String? title,
+    String? description,
+    String? imageUrl,
+  }) {
+    return HomeModel(
+      id: id ?? this.id,
+      title: title ?? this.title,
+      description: description ?? this.description,
+      imageUrl: imageUrl ?? this.imageUrl,
+    );
+  }
+
+  @override
+  String get collection => Collections.home;
+
+  @override
+  String get idModel => id;
+
+  @override
+  setIdModel(String id) {
+    return copyWith(id: id);
+  }
+
+  @override
+  Map<String, dynamic> toMap() {
+    return {
+      "id": id,
+      "title": title,
+      "description": description,
+      "imageUrl": imageUrl,
+    };
+  }
+}
---
+++ lib/features/restricted/models/restricted_model.dart
@@ -0,0 +1,78 @@
+import 'package:desafio_capyba/shared/constants/collections.dart';
+import 'package:desafio_capyba/shared/models/base_model.dart';
+
+class RestrictedModel extends BaseModel<RestrictedModel> {
+  final String id;
+  final String title;
+  final String description;
+  final String rarity;
+  final String imageUrl;
+
+  RestrictedModel({
+    required this.rarity,
+    required this.id,
+    required this.title,
+    required this.description,
+    required this.imageUrl,
+  });
+
+  factory RestrictedModel.empty() {
+    return RestrictedModel(
+      rarity: "",
+      id: "",
+      title: "",
+      description: "",
+      imageUrl: "",
+    );
+  }
+
+  @override
+  fromMap(Map<String, dynamic>? map) {
+    if (map == null) return RestrictedModel.empty();
+    return RestrictedModel(
+      rarity: map["rarity"] ?? "",
+      id: map["id"] ?? "",
+      title: map['title'] ?? "",
+      description: map['description'] ?? "",
+      imageUrl: map['imageUrl'] ?? "",
+    );
+  }
+
+  RestrictedModel copyWith({
+    String? id,
+    String? title,
+    String? description,
+    String? imageUrl,
+    String? rarity,
+  }) {
+    return RestrictedModel(
+      rarity: rarity ?? this.rarity,
+      id: id ?? this.id,
+      title: title ?? this.title,
+      description: description ?? this.description,
+      imageUrl: imageUrl ?? this.imageUrl,
+    );
+  }
+
+  @override
+  String get collection => Collections.home;
+
+  @override
+  String get idModel => id;
+
+  @override
+  setIdModel(String id) {
+    return copyWith(id: id);
+  }
+
+  @override
+  Map<String, dynamic> toMap() {
+    return {
+      "id": id,
+      "title": title,
+      "description": description,
+      "imageUrl": imageUrl,
+      "rarity": rarity,
+    };
+  }
+}
