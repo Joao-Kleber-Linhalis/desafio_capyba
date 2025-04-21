032f35286655286ca8ad5b6cde959f51cd060f5d
João Kleber Linhalis <jklinhalis@gmail.com>
2025-04-21 18:54:42 -0300

    imp generic screen

--- lib/features/home/presentation/home_screen.dart
+++ lib/features/home/presentation/home_screen.dart
@@ -1,5 +1,6 @@
+import 'package:desafio_capyba/features/home/models/home_model.dart';
 import 'package:desafio_capyba/features/home/provider/home_provider.dart';
-import 'package:desafio_capyba/shared/widgets/grid_widget.dart';
+import 'package:desafio_capyba/shared/screens/base_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -10,63 +11,17 @@ class HomeScreen extends StatefulWidget {
   State<HomeScreen> createState() => _HomeScreenState();
 }
 
-class _HomeScreenState extends State<HomeScreen> {
-  bool isLoading = true;
-  String searchText = '';
+class _HomeScreenState extends BaseScreen<HomeScreen, HomeProvider> {
+  @override
+  List<HomeModel> getList(HomeProvider provider) => provider.homeList;
 
   @override
   void initState() {
     super.initState();
-
     Provider.of<HomeProvider>(context, listen: false).loadhomeList().then((_) {
       setState(() {
         isLoading = false;
       });
     });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Consumer<HomeProvider>(
-      builder: (context, provider, child) {
-        if (isLoading) {
-          return const Center(child: CircularProgressIndicator());
-        }
-
-        // Filtrando a lista com base na busca
-        final filteredList = provider.homeList.where((item) {
-          return item.title.toLowerCase().contains(searchText.toLowerCase()) ||
-              item.description.toLowerCase().contains(searchText.toLowerCase());
-        }).toList();
-
-        return Column(
-          children: [
-            // 🔍 SearchBar no topo
-            Padding(
-              padding: const EdgeInsets.all(12.0),
-              child: TextField(
-                decoration: const InputDecoration(
-                  hintText: 'Buscar por título ou descrição...',
-                  prefixIcon: Icon(Icons.search),
-                  border: OutlineInputBorder(),
-                ),
-                onChanged: (value) {
-                  setState(() {
-                    searchText = value;
-                  });
-                },
-              ),
-            ),
-
-            // Lista filtrada
-            filteredList.isEmpty
-                ? const Center(child: Text("Nenhum item encontrado"))
-                : GridWidget(
-                    items: filteredList,
-                  ),
-          ],
-        );
-      },
-    );
   }
 }
--- lib/features/restricted/presentation/restricted_screen.dart
+++ lib/features/restricted/presentation/restricted_screen.dart
@@ -1,4 +1,8 @@
+import 'package:desafio_capyba/features/restricted/models/restricted_model.dart';
+import 'package:desafio_capyba/features/restricted/provider/restricted_provider.dart';
+import 'package:desafio_capyba/shared/screens/base_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class RestrictedScreen extends StatefulWidget {
   const RestrictedScreen({super.key});
@@ -7,11 +11,24 @@ class RestrictedScreen extends StatefulWidget {
   State<RestrictedScreen> createState() => _RestrictedScreenState();
 }
 
-class _RestrictedScreenState extends State<RestrictedScreen> {
+class _RestrictedScreenState
+    extends BaseScreen<RestrictedScreen, RestrictedProvider> {
   @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: Text("Restricted Screen"),
-    );
+  bool get needEmailVerified => true;
+
+  @override
+  List<RestrictedModel> getList(RestrictedProvider provider) =>
+      provider.userRestrictedList;
+
+  @override
+  void initState() {
+    super.initState();
+    Provider.of<RestrictedProvider>(context, listen: false)
+        .loadRestrictedList()
+        .then((_) {
+      setState(() {
+        isLoading = false;
+      });
+    });
   }
 }
